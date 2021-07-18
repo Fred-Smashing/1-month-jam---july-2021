e4e666e93db5be7b6b1940de36efa6c4ef75daa4
Fred-Smashing <59022894+Fred-Smashing@users.noreply.github.com>
2021-07-18 17:14:18 +0100

    added test background music

--- Assets/Scripts/Game Scene/GameManager.cs
+++ Assets/Scripts/Game Scene/GameManager.cs
@@ -30,6 +30,7 @@ public class GameManager : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.R))
         {
+            Time.timeScale = 1;
             Application.LoadLevel(0);
         }
     }
